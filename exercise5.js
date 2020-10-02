82f50841457ce265489eeae7e77a9ede653582dc
Wenlong Lu <yummybroccoli@outlook.com>
2020-10-02 17:24:14 +0100

    Exercise 5

--- exercise5.js
+++ exercise5.js
@@ -1,9 +1,9 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const uncompleted = json.reduce((cnt,todo) => {
-        todo.completed ? cnt.push({userId:todo.userId, title:todo.title}) : cnt;
-        return cnt;    
+     const uncompleted = json.reduce((acc,todo) => {
+        !todo.completed ? acc.push({userId:todo.userId, title:todo.title}) : acc;
+        return acc;    
      },[]) // Complete this code
      console.log(uncompleted)
   })
